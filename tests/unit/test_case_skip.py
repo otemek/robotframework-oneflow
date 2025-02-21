62dfb7cd0ed0369fb3b69f5a1693b4108b414088
otemek <etombsc@gmail.com>
2025-02-21 12:24:24 +0100

    add unit tests

--- tests/unit/test_case_skip.py
+++ tests/unit/test_case_skip.py
@@ -9,8 +9,15 @@ def test_data_dir() -> Path:
 
 
 @pytest.fixture
-def suite_w_tests_to_skip(test_data_dir) -> TestSuite:
-    suite_file = test_data_dir / "skip_test_case.robot"
+def suite_one(test_data_dir) -> TestSuite:
+    suite_file = test_data_dir / "skip_test_case_01.robot"
+    suite = TestSuite.from_file_system(suite_file)
+    return suite
+
+
+@pytest.fixture
+def suite_two(test_data_dir) -> TestSuite:
+    suite_file = test_data_dir / "skip_test_case_02.robot"
     suite = TestSuite.from_file_system(suite_file)
     return suite
 
@@ -22,12 +29,21 @@ def suite_all_tests_skipped(test_data_dir) -> TestSuite:
     return suite
 
 
-def test_after_failed_test_other_tests_should_be_skipped(
-    suite_w_tests_to_skip, tmp_path
+def test_whole_suite_should_be_skipped_when_parent_setup_fail(
+    suite_all_tests_skipped, tmp_path
 ):
     temp_output = tmp_path / "output.xml"
-    result = suite_w_tests_to_skip.run(output=temp_output)
-    assert result.suite.name == "Skip Test Case"
+    result = suite_all_tests_skipped.run(output=temp_output)
+    assert result.suite.name == "Skip Suite"
+    skipped_tests = result.suite.tests
+    for test in skipped_tests:
+        assert test.status == "SKIP"
+
+
+def test_after_failed_test_other_tests_should_be_skipped(suite_one, tmp_path):
+    temp_output = tmp_path / "output.xml"
+    result = suite_one.run(output=temp_output)
+    assert result.suite.name == "Skip Test Case 01"
     passed_test = result.suite.tests[0]
     assert passed_test.status == "PASS"
     failed_test = result.suite.tests[1]
@@ -37,12 +53,17 @@ def test_after_failed_test_other_tests_should_be_skipped(
         assert test.status == "SKIP"
 
 
-def test_whole_suite_should_be_skipped_when_parent_setup_fail(
-    suite_all_tests_skipped, tmp_path
+def test_another_suite_should_be_executed_if_previous_suite_has_failed(
+    suite_two, tmp_path
 ):
     temp_output = tmp_path / "output.xml"
-    result = suite_all_tests_skipped.run(output=temp_output)
-    assert result.suite.name == "Skip Suite"
-    skipped_tests = result.suite.tests
+    result = suite_two.run(output=temp_output)
+    assert result.suite.name == "Skip Test Case 02"
+    passed_tests = result.suite.tests[:2]
+    for test in passed_tests:
+        assert test.status == "PASS"
+    failed_test = result.suite.tests[2]
+    assert failed_test.status == "FAIL"
+    skipped_tests = result.suite.tests[3:]
     for test in skipped_tests:
         assert test.status == "SKIP"
